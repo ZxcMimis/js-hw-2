901386daa92137ea2d2e313fa0f7748635114b27
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2024-05-25 01:21:16 +0300

    js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const fahrenheit = 77;
+const celsius = 25;
+console.log("1 excersise");
+console.log(((fahrenheit - 32) * 5) / 9);
+
+
+console.log("2 excersise");
+const month = 31;
+const monthHours = 744;
+const monthMinute = 1440;
+
+console.log("In 31 days :", monthHours, "hours");
+console.log("In 31 days :", monthMinute, "minutes");
+
+
+const health = 100;
+const energy = 100;
+
+console.log("3 excersise");
+console.log("Your health is:", health - 34);
+console.log("Your energy is:", energy - 71);
+
+console.log("4 excersise");
+const buyPrice = 9365;
+console.log(buyPrice * 0.1);
+
+
+console.log("5 excersise");
+const numberComa = 199.6;
+console.log(Math.floor(numberComa))
+
+
+console.log("6 excersise");
+const biggerNumberString = "9.6";
+console.log(parseFloat(biggerNumberString));
+
+
+console.log("7 excersise");
+const parseIntNumber = "9";
+console.log(parseInt(parseIntNumber));
+
+
+console.log("8 excersise");
+const sqrt = 169;
+console.log(Math.sqrt(sqrt))
+
+
+console.log("9 excersise");
+const integer = 100;
+const stringNumber = "100";
+
+console.log(parseInt(stringNumber));
+console.log(integer.toString());
